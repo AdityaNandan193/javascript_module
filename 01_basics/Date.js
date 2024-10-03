db567bb7167c857b96f135463b9a10c502997492
AdityaNandan193 <115461905+AdityaNandan193@users.noreply.github.com>
2024-10-03 17:59:46 +0000

    working on dates

---
+++ 01_basics/Date.js
@@ -0,0 +1,14 @@
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+console.log(typeof(myDate)) // it is a date object
+
+let myCreatedDate = new Date(2024, 9, 3 , 11, 25, 40)
+console.log(myCreatedDate.toLocaleString())
+// in javascript january starts from the index 0
