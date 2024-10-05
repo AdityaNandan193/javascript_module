337dd3a4091ae60dae27ff03cc865c282225b7bd
AdityaNandan193 <115461905+AdityaNandan193@users.noreply.github.com>
2024-10-05 06:31:13 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expressions(IIFE)
+
+// TO REMOVE THE POLLUTION FROM THE GLOBAL SCORE IIFE IS USED
+// AND TO IMMEDIATELY INVOKE THE FUNCTION ALSO
+
+// syntax :- ()() so first bracket is for definition of function
+// second bracket is used as it is for execution purpose
+
+(function one(){
+    // name iife because it has name one
+    console.log(`db connected`)
+})(); // this semicolon is important because it ends the context 
+// then only next function will be able to execute 
+
+( (name) => {
+    // unnamed iife
+    console.log(`db connected two ${name}`)
+}) ('hitesh');
