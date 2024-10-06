cac2457d31a70671d4503ff41090704477ff8a45
AdityaNandan193 <115461905+AdityaNandan193@users.noreply.github.com>
2024-10-06 19:08:40 +0000

    while and dowhile loop

---
+++ iterations/while_and_dowhile.js
@@ -0,0 +1,19 @@
+let arr = ["flash" , "batman" ,"ironman"]
+
+let i = 0;
+while(i<arr.length){
+    console.log(`value is ${arr[i]}`);
+    i++;
+
+}
+
+// do while
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`)
+    score++
+} while(score >= 10)
+
+    // do while print atleast one iteration
